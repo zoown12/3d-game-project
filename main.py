39d8d657756f402f890016d946a686c7ba6244d6
zoown12 <zoown12@gmail.com>
2022-11-29 01:24:12 +0900

    NPC 총 맞는 모션까지 완료. NPC 사망까지 완료. NPC 추적 중... NPC 추적 완료. HEALTH 완료.

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import pygame,pygame_menu
+import pygame
 import sys
+import pygame_menu
 from settings1 import *
 from map1 import *
 from raycasting import *
@@ -11,53 +12,54 @@ from weapon import *
 from sound import *
 from pathfinding import *
 
+
 class Game:
     def __init__(self):
-        pygame.init()
-        pygame.mouse.set_visible(False) #마우스 포인트 숨기기
+        pygame.mouse.set_visible(False)  # 마우스 포인트 숨기기
         self.screen = pygame.display.set_mode(RES)
         self.clock = pygame.time.Clock()
         self.delta_time = 1
         self.global_trigger = False
         self.global_event = pygame.USEREVENT + 0
-        pygame.time.set_timer(self.global_event,40) #타이머 40밀리초 설정
+        pygame.time.set_timer(self.global_event, 40)  # 타이머 40밀리초 설정
         self.new_game()
-        
+
     def new_game(self):
-        self.map = Map(self)
+        self.map = Map(self, value=SELECT_MAP)
         self.player = Player(self)
         self.object_renderer = ObjectRenderer(self)
         self.raycasting = RayCasting(self)
         # self.static_sprite = SpriteObject(self)
         # self.animated_sprite = AnimatedSprite(self)
         self.object_handler = ObjectHandler(self)
-        self.weapon=Weapon(self)
-        self.sound=Sound(self)
+        self.weapon = Weapon(self)
+        self.sound = Sound(self)
         self.pathfinding = PathFinding(self)
-    
+        pg.mixer.music.play(-1)
+
     def update(self):
         self.player.update()
         self.raycasting.update()
-        
-        #object_renderer 완성 후 실행
+
+        # object_renderer 완성 후 실행
         # self.static_sprite.update()
         # self.animated_sprite.update()
-    
+
         self.object_handler.update()
-        self.weapon.update() 
-        
+        self.weapon.update()
+
         pygame.display.flip()
         self.delta_time = self.clock.tick(FPS)
-        #self.clock.tick(0)
+        # self.clock.tick(0)
         pygame.display.set_caption(f'{self.clock.get_fps() :.1f}')
-        
+
     def draw(self):
-        #self.screen.fill('black')
+        # self.screen.fill('black')
         self.object_renderer.draw()
-        self.weapon.draw()
-        #self.map.draw()
-        #self.player.draw()
-        
+        self.weapon.draw
+        # self.map.draw()
+        # self.player.draw()
+
     def check_events(self):
         self.global_trigger = False
         for event in pygame.event.get():
@@ -67,14 +69,47 @@ class Game:
             elif event.type == self.global_event:
                 self.global_trigger = True
             self.player.single_fire_event(event)
-                
-                
+
     def run(self):
         while True:
             self.check_events()
             self.update()
             self.draw()
 
+
+class Menu:
+    def __init__(self):
+        pygame.init()
+
+    def level(self, trash, value2):  # 난이도 선택시 호출되는 함수
+        print("난이도 선택값:", trash)
+        print("value2", value2)
+        global SELECT_MAP
+        SELECT_MAP = value2
+
+    def start(self):  # 게임시작 선택시 호출되는 함수
+        print("게임시작")
+        game = Game()
+        game.run()
+
+    def quit():
+        pygame.quit()
+        sys.exit()
+
+    def run(self):
+        global SELECT_MAP
+        SELECT_MAP = 1
+        surface = pygame.display.set_mode((600, 400))
+        t = pygame_menu.themes.THEME_DARK
+        t.widget_font = pygame.font.SysFont("gothic", 30)
+
+        menu = pygame_menu.Menu("DOOM", 400, 300, theme=t)
+        menu.add.selector("Map ", [("1", 1), ("2", 2)], onchange=self.level)
+        menu.add.button("Start", self.start)
+        menu.add.button("Quit", quit)
+        menu.mainloop(surface)
+
+
 if __name__ == '__main__':
-    game = Game()
-    game.run()
+    menu = Menu()
+    menu.run()
--- object_handler.py
+++ object_handler.py
@@ -1,16 +1,23 @@
 from sprite_object1 import *
 from npc import *
+from random import choices, randrange
 class ObjectHandler:
     def __init__(self, game):
         self.game = game
         self.sprite_list = []
         self.npc_list = []
-        self.npc_sprite_path ='resources/sprites/npc'
-        self.static_sprite_path = 'resources/sprites/static_sprites'
-        self.anim_sprite_path = 'resources/sprites/animated_sprites'
+        self.npc_sprite_path = 'resources/sprites/npc/'
+        self.static_sprite_path = 'resources/sprites/static_sprites/'
+        self.anim_sprite_path = 'resources/sprites/animated_sprites/'
         add_sprite = self.add_sprite
         add_npc = self.add_npc
         self.npc_positions = {}
+
+        self.enemies = 20  # npc count
+        self.npc_types = [SoldierNPC, CacoDemonNPC, CyberDemonNPC]
+        self.weights = [70, 20, 10]
+        self.restricted_area = {(i, j) for i in range(10) for j in range(10)}
+        self.spawn_npc()
 
         # 맵에 구조물 생성
         add_sprite(SpriteObject(game))
@@ -27,13 +34,26 @@ class ObjectHandler:
         #add_sprite(AnimatedSprite(game, path=self.anim_sprite_path + 'red_light/0.png', pos=(9.5, 7.5)))
 
         #npc map
-        add_npc(NPC(game))
-        add_npc(NPC(game,pos=(11.5,4.5)))
-
+        #add_npc(NPC(game))
+        #add_npc(NPC(game,pos=(11.5,4.5)))
+    def check_win(self):
+        if not len(self.npc_positions):
+            self.game.object_renderer.win()
+            pg.display.flip()
+            pg.time.delay(1500)
+            self.game.new_game()
+    def spawn_npc(self):
+        for i in range(self.enemies):
+                npc = choices(self.npc_types, self.weights)[0]
+                pos = x, y = randrange(self.game.map.cols), randrange(self.game.map.rows)
+                while (pos in self.game.map.world_map) or (pos in self.restricted_area):
+                    pos = x, y = randrange(self.game.map.cols), randrange(self.game.map.rows)
+                self.add_npc(npc(self.game, pos=(x + 0.5, y + 0.5)))
     def update(self):
         self.npc_positions = {npc.map_pos for npc in self.npc_list if npc.alive} #적 NPC 겹치지 않게
         [sprite.update() for sprite in self.sprite_list]
         [npc.update() for npc in self.npc_list]
+        self.check_win()
 
     def add_npc(self,npc):
         self.npc_list.append(npc)
